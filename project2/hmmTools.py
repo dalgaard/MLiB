8f6360f4ea856190c833677521ef41ac1f3c3cc1
Patrick Ettenhuber <pettenhuber@gmail.com>
2017-02-16 21:55:09 +0100

    added forward and backward passes

--- project2/hmmTools.py
+++ project2/hmmTools.py
@@ -2,7 +2,7 @@ from math import log
 
 class Hmm(object):
     
-    __slots__ = ['hidden', 'observables', 'pi', 'A', 'emissions']
+    __slots__ = ['hidden', 'observables', 'pi', 'A', 'emissions','K','N','alpha','beta','c']
     
     def __init__(self, file):
         self.hidden = []
@@ -29,6 +29,11 @@ class Hmm(object):
                 i += 2+len(self.hidden)
             else:
                 i += 1
+        self.K = len(self.pi)
+        self.N = len(self.emissions[0])
+        self.alpha = []
+        self.beta = []
+        self.c = []
 
     def logLikelihood(self, hiddenSequence, observedSequence):
         hidden_index = [ self.hidden.index(h) for h in hiddenSequence ]
@@ -41,3 +46,35 @@ class Hmm(object):
                 ll += log(self.A[hidden_index[i-1]][hidden_index[i]])
             ll += log(self.emissions[hidden_index[i]][observed_index[i]])
         return ll
+    
+    def forward(self):
+        # initialize
+        delta = [ self.pi[k] * self.emissions[k][0] for k in range(self.K) ]
+        self.c = [ sum(delta) if n== 0 else 0.0 for n in range(self.N) ]
+        self.alpha = [[ delta[k]/self.c[n] if n==0 else 0.0 for n in range(self.N) ] for k in range(self.K) ]
+        
+        # calculate subsequent steps
+        for n in range(1,self.N):
+            delta = [ 0.0 for k in range(self.K) ]
+            for k in range(self.K):
+                for kk in range(self.K):
+                    delta[k] += self.alpha[kk][n-1] * self.A[k][kk]
+                delta[k] = self.emissions[k][n] * delta[k]
+            self.c[n] = sum(delta)
+            for k in range(self.K):
+                self.alpha[k][n] = delta[k] / self.c[n]
+    
+    def backward(self):
+        # initialize
+        self.beta = [[ 1.0 if n==self.N-1 else 0.0 for n in range(self.N) ] for k in range(self.K) ]
+        
+        # calculate subsequent steps
+        for n in range(self.N-2,-1,-1):
+            delta = [ 0.0 for k in range(self.K) ]
+            for k in range(self.K):
+                for kk in range(self.K):
+                    delta[k] += self.beta[kk][n+1] * self.A[kk][k] * self.emissions[kk][n+1]
+            for k in range(self.K):
+                self.beta[k][n] = delta[k] / self.c[n+1]
+        
+            
